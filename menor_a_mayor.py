96bd4e59d3ae6f646b6f6b6fa2b804ca538c938e
Matías <matiasayaman@gmail.com>
2018-01-10 08:11:22 -0300

    Mini proyectom Tercera clase con nate

---
+++ menor_a_mayor.py
@@ -0,0 +1,17 @@
+
+
+print("Primer proyecto")
+
+dato_uno = input("dime un numero")
+
+dato_dos = input("di otro numero")
+
+print("El Nummero mayor es:")
+
+print(max(dato_uno, dato_dos))
+
+print ("El Numero menor es:")
+
+print(min(dato_uno, dato_dos))
+
+
