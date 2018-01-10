b8f60dd3f985f1b4d9023618769c4afa8a10d023
Matías <matiasayaman@gmail.com>
2018-01-10 08:12:20 -0300

    Añadiendo el archivo hello

---
+++ test_input.py
@@ -0,0 +1,19 @@
+
+print("MENOR A MAYOR ")
+
+
+dato_no_uno =input("dime un numero")
+
+dato_no_dos = input("dime otro numero")
+
+print("el numero mayor es")
+print(max(dato_no_uno, dato_no_dos))
+
+
+
+print("El numero menor es")
+print(min(dato_no_uno, dato_no_dos))
+
+print("Primer mini proyecto:D")
+
+
